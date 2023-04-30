80ee3ef805787f1a4ef405e9d5d57a695d5d3b7f
J-hurtado94 <juanhurtado2994@gmail.com>
2023-04-30 16:33:35 -0500

    delete jenkins file

--- cypress/e2e/upload-file.cy.ts
+++ cypress/e2e/upload-file.cy.ts
@@ -1,9 +1,9 @@
 
-describe('Upload file', () => {
-    beforeEach(() => {
-        cy.visit(Cypress.env('demoQA')+"/upload-download")
-    })
-    it('upload file from fixture', () => {
-        cy.get('#uploadFile').selectFile('C:/Users/USUARIO/Desktop/git_entry.png')
-    });
-});
+// describe('Upload file', () => {
+//     beforeEach(() => {
+//         cy.visit(Cypress.env('demoQA')+"/upload-download")
+//     })
+//     it('upload file from fixture', () => {
+//         cy.get('#uploadFile').selectFile('C:/Users/USUARIO/Desktop/git_entry.png')
+//     });
+// });
